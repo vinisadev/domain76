6a1c9e6efe13fcb592bdd153b374ae1098abdbad
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-08 17:19:28 -0500

    Add grid background to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <div
+      className="absolute inset-0 -z-10 h-full w-full bg-white
+        bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)]
+        bg-[size:14px_24px]"
+    >
+      Hello world!
+    </div>
+  );
 }
