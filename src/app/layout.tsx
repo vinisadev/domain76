54926a3b74bc4b882c12f854e1f314e14da081af
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-08 17:15:42 -0500

    Updated favicon and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const font = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "domain76",
+  title: "domain76 | Design & Development Studio",
   description:
     "Grow your business with domain76, a design & development studio.",
 };
