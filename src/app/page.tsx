2d91f342ea6e23ee63c4129bae7304700e44638c
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-09 18:02:41 -0500

    Closing out main hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import LogoImage from "../public/logo/logo.webp";
 import Link from "next/link";
 import { Link as ScrollLink, Element } from "react-scroll";
 import { GoogleReviews } from "@/components/demos/google-reviews";
-import { Hero } from "@/components/demos/hero";
+import { CoverSection } from "@/components/demos/cover-section";
 
 export default function Home() {
   return (
@@ -77,7 +77,26 @@ export default function Home() {
       <main className="md:pb-10">
         <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto mt-14">
           <GoogleReviews />
-          <Hero />
+        </div>
+        <h1>
+          <CoverSection />
+        </h1>
+        <p className="md:text-center text-xl md:text-2xl my-6 md:my-10 md:w-4/5 mx-auto text-gray-500">
+          Schedule a call with us to discuss your project and get a quote.
+        </p>
+        <div className="flex md:justify-center items-center gap-x-4">
+          <Link
+            href="/meeting"
+            className="py-3 px-10 md:px-16 md:text-xl hover:bg-[#abcbff] rounded-[6px] border-2 border-black dark:border-white bg-[#121212] text-white transition duration-200 hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+          >
+            Book a Call
+          </Link>
+          <Link
+            href={"/showcase"}
+            className="bg-white py-3 px-10 md:px-16 md:text-xl border-4 border-black rounded-[6px] hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+          >
+            Showcase
+          </Link>
         </div>
       </main>
     </div>
--- src/components/demos/google-reviews.tsx
+++ src/components/demos/google-reviews.tsx
@@ -11,23 +11,8 @@ export function GoogleReviews() {
           "group rounded-full text-base text-white transition-all ease-in"
         )}
       >
-        <AnimatedShinyText
-          className=" 
-          items-center 
-          justify-center 
-          py-1 transition ease-out
-          hover:text-neutral-600 
-          hover:duration-300
-          hover:dark:text-neutral-400"
-        >
-          <div
-            className="
-          md:flex
-          flex-col-reverse
-          md:flex-row
-          justify-between
-          gap-x-6"
-          >
+        <AnimatedShinyText className="items-center justify-center py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+          <div className="md:flex flex-col-reverse md:flex-row justify-between gap-x-6">
             ⭐ ⭐ ⭐ ⭐ ⭐
             <div className="flex items-center space-x-2 py-2 md:py-0">
               <FcGoogle className="h-6 w-6 " />
