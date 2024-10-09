d8c448e39647260d80421aba42b50be637ff18b8
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-09 18:30:27 -0500

    Added infinite scrolling logo to statsistics section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { GoogleReviews } from "@/components/demos/google-reviews";
 import { CoverSection } from "@/components/demos/cover-section";
 import BoxReveal from "@/components/ui/box-reveal";
 import { PiCheckBold } from "react-icons/pi";
+import NumberTicker from "@/components/ui/number-ticker";
+import { InfiniteMovingLogos } from "@/components/ui/infinite-moving-logos";
 
 export default function Home() {
   return (
@@ -79,52 +81,135 @@ export default function Home() {
       <main className="md:pb-10">
         <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto mt-14">
           <GoogleReviews />
-        </div>
-        <h1>
-          <CoverSection />
-        </h1>
-        <p className="md:text-center text-xl md:text-2xl my-6 md:my-10 md:w-4/5 mx-auto text-gray-500">
-          Schedule a call with us to discuss your project and get a quote.
-        </p>
-        <div className="flex md:justify-center items-center gap-x-4">
-          <Link
-            href="/meeting"
-            className="py-3 px-10 md:px-16 md:text-xl hover:bg-[#abcbff] rounded-[6px] border-2 border-black dark:border-white bg-[#121212] text-white transition duration-200 hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+
+          <h1>
+            <CoverSection />
+          </h1>
+          <p
+            className="md:text-center
+           text-xl md:text-2xl my-6 md:my-10 md:w-4/5 mx-auto text-gray-500"
           >
-            Book a Call
-          </Link>
-          <Link
-            href={"/showcase"}
-            className="bg-white py-3 px-10 md:px-16 md:text-xl border-4 border-black rounded-[6px] hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+            Schedule a call with us to discuss your project and get a quote in
+            minutes
+          </p>
+
+          <div
+            className="
+                 flex 
+                  md:justify-center 
+                  items-center 
+                  gap-x-4
+                   "
           >
-            Showcase
-          </Link>
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-centered text-left md:justify-items-center md:mx-auto mt-10 md:mt-16">
-          <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
-            <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
-              <PiCheckBold className="text-xl text-blue-500" />
-              Design
-            </p>
-          </BoxReveal>
-          <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
-            <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
-              <PiCheckBold className="text-xl text-blue-500" />
-              Development
-            </p>
-          </BoxReveal>
-          <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
-            <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
-              <PiCheckBold className="text-xl text-blue-500" />
-              Marketing
-            </p>
-          </BoxReveal>
-          <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
-            <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
-              <PiCheckBold className="text-xl text-blue-500" />
-              Strategy
-            </p>
-          </BoxReveal>
+            <Link
+              href="/meeting"
+              className="py-3 
+            px-10
+            md:px-16
+      md:text-xl
+      hover:bg-[#abcbff] 
+      rounded-[6px]
+      border-2 
+      border-black 
+      dark:border-white 
+       bg-[#121212] 
+       text-white 
+       transition 
+       duration-200 
+       hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+            >
+              Book a Call
+            </Link>
+            <Link
+              href={"/showcase"}
+              className="
+              bg-white
+   py-3 
+   px-10
+   md:px-16
+      md:text-xl
+        border-4
+        border-black
+        rounded-[6px]
+        hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+            >
+              Showcase
+            </Link>
+          </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-center text-left md:justify-items-center md:mx-auto mt-10 md:mt-16">
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Design
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Development
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Marketing
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={"#3b82f6"} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Strategy
+              </p>
+            </BoxReveal>
+          </div>
+
+          <div className="md:flex items-center justify-between gap-y-4 my-10 gap-x-28 mx-auto">
+            <div className="md:w-2/5">
+              <h1 className="text-2xl font-medium text-gray-600 w-4/5">
+                Trusted by fast moving brands worldwide
+              </h1>
+
+              <div className="flex my-6 gap-x-5 w-full">
+                <div>
+                  <h1 className="text-blue-500 text-3xl md:text-5xl">
+                    <NumberTicker value={1000} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Happy Clients
+                    </p>
+                  </h1>
+                </div>
+
+                <div className="w-px bg-gray-300 self-stretch"></div>
+
+                <div className="flex-1 min-w-0">
+                  <h1 className="text-blue-500 text-3xl md:text-5xl whitespace-nowrap overflow-hidden">
+                    <NumberTicker value={100} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Projects Completed
+                    </p>
+                  </h1>
+                </div>
+              </div>
+            </div>
+
+            <section className="overflow-hidden mt-10 md:w-4/5">
+              <InfiniteMovingLogos
+                speed="slow"
+                direction="left"
+                items={[
+                  {
+                    logo: "/logo/logo.webp",
+                    name: "Logo",
+                  },
+                  {
+                    logo: "/logo/logo.webp",
+                    name: "Logo",
+                  },
+                ]}
+              />
+            </section>
+          </div>
         </div>
       </main>
     </div>
