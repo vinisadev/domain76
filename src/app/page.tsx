96a7ad8aec7d77b8e76b16012eb2cfb54ce453b3
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-10 02:56:28 -0500

    Added footer component to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import BoxRevealDemo from "@/components/demos/box-reveal-section";
 import { IconStarFilled } from "@tabler/icons-react";
 import { GuaranteesSection } from "@/components/demos/guarantees-section";
 import LetsMakeThingsHappenSection from "@/components/ui/lets-make-things-happen";
+import Footer from "@/components/footer";
 
 const services = [
   {
@@ -370,28 +371,7 @@ export default function Home() {
         <LetsMakeThingsHappenSection />
       </section>
 
-      <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
-        <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
-          <h1 className="text-3xl md:text-5xl font-medium ">
-            <Image
-              src={"/logo/logo.webp"}
-              width={10000}
-              height={10000}
-              className="w-40"
-              alt="image"
-            />{" "}
-          </h1>
-          <p className="text-left  text-xl  text-gray-500">(346) 298-2112</p>
-          <p className="text-left  text-xl  text-gray-500">info@domain76.com</p>
-        </div>
-
-        <div className="flex md:justify-center gap-x-4 mt-10">
-          © 2025 domain76. All Rights Reserved.
-          <Link href="/" className="text-blue-500">
-            Privacy Policy
-          </Link>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
