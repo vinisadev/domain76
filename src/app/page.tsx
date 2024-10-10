924c46f3db67f5e8d6ac94adf90c4df09a1b2900
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-09 19:10:48 -0500

    Added services component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,51 @@ import BoxReveal from "@/components/ui/box-reveal";
 import { PiCheckBold } from "react-icons/pi";
 import NumberTicker from "@/components/ui/number-ticker";
 import { InfiniteMovingLogos } from "@/components/ui/infinite-moving-logos";
+import { HeadingPullUp } from "@/components/demos/heading-pull-up";
+
+const services = [
+  {
+    icon: "/images/s_6.png",
+    title: "Web Design + Development",
+    description:
+      "Take your business to the next level with our web design and development services",
+  },
+  {
+    icon: "/images/s_1.png",
+    title: "Search Engine Optimization",
+    description:
+      "Get your website to the top of search engine results with our SEO services",
+  },
+  {
+    icon: "/images/s_5.png",
+    title: "Content Creation",
+    description:
+      "Boost your brand's online presence with our social media marketing services",
+  },
+  {
+    icon: "/images/s_3.png",
+    title: "Social Media Marketing",
+    description:
+      "Interact with your customers and increase sales with our email marketing services",
+  },
+  {
+    icon: "/images/s_4.png",
+    title: "Email Marketing",
+    description:
+      "With our content creation services, we help businesses drive results",
+  },
+  {
+    icon: "/images/s_2.png",
+    title: "Pay-Per-Click Advertising",
+    description:
+      "Don't waste money on ineffective advertising. Our PPC services help you reach your target audience",
+  },
+];
 
 export default function Home() {
   return (
     <div
-      className="overflow-clip inset-0 -z-10 h-screen w-full bg-[#fafafa]
+      className="overflow-clip inset-0 -z-10 h-full w-full bg-[#fafafa]
         bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)]
         bg-[size:14px_24px]"
     >
@@ -204,7 +244,7 @@ export default function Home() {
                   },
                   {
                     logo: "/logo/logo.webp",
-                    name: "Logo",
+                    name: "Logo2",
                   },
                 ]}
               />
@@ -212,6 +252,36 @@ export default function Home() {
           </div>
         </div>
       </main>
+
+      <Element name="services">
+        <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto ">
+          <h1>
+            <HeadingPullUp />
+          </h1>
+          <p className="md:text-center py-4 md:w-1/2 mx-auto text-xl md:text-2xl text-gray-500">
+            All of our services are designed to help your business stand out
+          </p>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10">
+            {services.map((service) => (
+              <div
+                key={service.title}
+                className="flex flex-col justify-between h-full space-y-4 text-center bg-gray-100 p-4 cursor-pointer hover:scale-105 transition-transform rounded-md"
+              >
+                <Image
+                  src={service.icon}
+                  width={10000}
+                  height={10000}
+                  className="object-contain bg-gray-100 p-4 w-full h-40 rounded-md"
+                  alt="image"
+                />
+                <h1 className="text-xl font-medium">{service.title}</h1>
+                <p className="text-gray-500">{service.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Element>
     </div>
   );
 }
---
+++ src/components/demos/heading-pull-up.tsx
@@ -0,0 +1,10 @@
+import WordPullUp from "@/components/ui/word-pull-up";
+
+export function HeadingPullUp() {
+  return (
+    <WordPullUp
+      className="text-3xl md:text-5xl md:text-center font-medium"
+      words="Built from the ground up"
+    />
+  );
+}
