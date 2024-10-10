31fb5694437fe8b81ffa16d0f4fac3de137343e3
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-09 23:20:40 -0500

    More section demos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,8 @@ import { HeadingPullUp } from "@/components/demos/heading-pull-up";
 import { ScrollBasedVelocityDemo } from "@/components/demos/scroll-based-velocity";
 import { AnimatedBeamSection } from "@/components/demos/animated-beam-section";
 import BoxRevealDemo from "@/components/demos/box-reveal-section";
+import { IconStarFilled } from "@tabler/icons-react";
+import { GuaranteesSection } from "@/components/demos/guarantees-section";
 
 const services = [
   {
@@ -325,6 +327,42 @@ export default function Home() {
             </div>
           </div>
         </main>
+      </Element>
+
+      <section>
+        <main className="md:flex items-center justify-center space-y-6 md:space-y-0 md:gap-x-20 xl:w-4/5 2xl:w-[68%] mx-auto px-6 md:px-0">
+          <Image
+            src={"/logo/logo.webp"}
+            width={10000}
+            height={10000}
+            className=" md:w-1/3 rounded-md"
+            alt="image"
+          />
+          <div className="flex flex-col gap-y-5 md:w-1/2">
+            <h1 className="text-lg md:text-2xl ">
+              &quot;We&apos;ve been working with domain76 for over 2 years and
+              they&apos;ve been amazing to work with. They&apos;ve helped us
+              grow our business and we couldn&apos;t be happier with the
+              results. &quot;
+            </h1>
+            <div className="flex items-center gap-x-1">
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+            </div>
+
+            <span className="text-xl font-medium">
+              Vincenzo Fehring <br />
+              CEO, Vend-Trak, Inc.
+            </span>
+          </div>
+        </main>
+      </section>
+
+      <Element name="guarantees">
+        <GuaranteesSection />
       </Element>
     </div>
   );
