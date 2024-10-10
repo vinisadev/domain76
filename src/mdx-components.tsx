734c10f23068e1a9c46bb7887dcff2d7b452d038
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-10 02:29:19 -0500

    Configured mdx and converted terms and conditions to mdx for ease of management

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,12 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h3: ({ children }) => {
+      return <h3 className="text-semibold text-2xl pt-5">{children}</h3>;
+    },
+    p: ({ children }) => {
+      return <p className="pt-5">{children}</p>;
+    },
     ...components,
   };
 }
